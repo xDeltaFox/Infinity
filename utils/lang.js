962c3e99b60734ff88e1b62fd99bc577ba617f86
xDeltaFox <Israelbz25@gmail.com>
2017-07-23 13:11:34 -0300

    Add files via upload
    Modulo de tradução

--- utils/lang.js
+++ utils/lang.js
@@ -1,29 +1,27 @@
 let fs = require('fs');
-let flatten = require('flat');
 
 function getlang(lang, key) {
-    //var language = JSON.parse(fs.readFileSync(`./lang/${lang}.json`, 'utf8'));
-    var language = {};
-
-    fs.readdir('./lang', (err, files) => {
-        if (err) {
-            console.log('Error reading langfiles', { attach: err });
-        } else {
-            files.forEach((file) => {
-                try {
-                    let f = require('./lang/' + file);
-                    if (file == lang) {
-                        language[f.lang] = flatten(f.strings);
-                    }
-                } catch (e) {
-                    console.log('Error reading langfile ' + file, { attach: e });
-                }
-            });
-        }
-    });
-
-    console.log(language);
-    return language + `.${key}`;
+    var valor;
+    switch (lang) {
+        case 'en':
+        case 'en-us':
+            //console.log('en-us');
+            var language = JSON.parse(fs.readFileSync(`./utils/lang/en-us.json`));
+            valor = eval("language." + key);
+            break;
+        case 'pt':
+        case 'pt-br':
+            //console.log('pt-br');
+            var language = JSON.parse(fs.readFileSync(`./utils/lang/pt-br.json`));
+            valor = eval("language." + key);
+            break;
+        default:
+            //console.log('dev');
+            var language = JSON.parse(fs.readFileSync(`./utils/lang/pt-br.json`));
+            valor = eval("language." + key);
+            break;
+    }
+    return valor;
 }
 
 module.exports = getlang;
